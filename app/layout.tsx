8bc84d6901bfdb30b7ed920751d6bbc8fd7a3dfc
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2023-11-22 16:07:42 +0530

    Search Modal and filters added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
