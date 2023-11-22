89700f133e0a5e63aab7abc053fe2ab91b3f9bc4
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2023-11-22 16:51:25 +0530

    force dynamic added

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
+import getListings, { IListingsParams } from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
   searchParams: IListingsParams;
 }
 
-export default async function Home({ searchParams }: HomeProps) {
+export const dynamic = "force-dynamic";
+
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
-  if (listings.length === 0 ) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -26,7 +28,8 @@ export default async function Home({ searchParams }: HomeProps) {
     <ClientOnly>
       <Container>
         <div
-          className=" pt-24
+          className="
+            pt-24
             grid 
             grid-cols-1 
             sm:grid-cols-2 
@@ -34,7 +37,8 @@ export default async function Home({ searchParams }: HomeProps) {
             lg:grid-cols-4
             xl:grid-cols-5
             2xl:grid-cols-6
-            gap-8"
+            gap-8
+          "
         >
           {listings.map((listing: any) => (
             <ListingCard
@@ -47,4 +51,6 @@ export default async function Home({ searchParams }: HomeProps) {
       </Container>
     </ClientOnly>
   );
-}
+};
+
+export default Home;
